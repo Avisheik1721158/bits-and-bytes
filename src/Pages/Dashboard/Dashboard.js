8907f5f4a61fe8a2773babec24cf6f2bfd2ccbf2
Avisheikbarua1158 <avisheik.barua@northsouth.edu>
2022-05-26 21:24:59 +0600

    dashoard

---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2>
+                This is dashboard
+            </h2>
+        </div>
+    );
+};
+
+export default Dashboard;
